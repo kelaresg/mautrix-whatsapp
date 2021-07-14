968ee3ed90df5a50adf92e10e5ebce482a76018d
zhaoYangguang <1163765691@qq.com>
2021-07-14 19:31:35 +0800

    do not sync the room that use has left

--- user.go
+++ user.go
@@ -871,10 +871,10 @@ func (user *User) syncPortals(chatMap map[string]whatsapp.Chat, createAll bool) 
 		create := (chat.LastMessageTime >= user.LastConnection && user.LastConnection > 0) || i < limit
 		if len(chat.Portal.MXID) > 0 || create || createAll {
 			user.log.Debugfln("Syncing chat %+v", chat.Chat.Source)
-			val, ok := chat.Chat.Source["read_only"]
+			_, ok := chat.Chat.Source["read_only"]
 			if ok {
 				chat.Portal.setReadOnly(true)
-				fmt.Println("Syncing portals 0000: ", val)
+				break
 			}
 			justCreated := len(chat.Portal.MXID) == 0
 			user.syncPortal(chat)
